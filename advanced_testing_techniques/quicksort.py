eb9418bf7b20c7ad3e03158138caeaa2b3df80ea
Frank Schmitt <Frank Schmitt schmitt@quattro-research.com>
2024-11-03 20:59:58 +0100

    finished (buggy) quicksort implementation and unit tests

--- advanced_testing_techniques/quicksort.py
+++ advanced_testing_techniques/quicksort.py
@@ -7,6 +7,12 @@ class Quicksort:
 
     def sort(self, input):
         """Sort the input list, and return the sorted list (input is unchanged)"""
-        return input
+        if len(input) <= 1:
+            return input
+        else:
+            pivot = input[0]
+            lt = [x for x in input if x < pivot]
+            gt = [x for x in input if x > pivot]
+            return self.sort(lt) + [pivot] + self.sort(gt)
     
     
--- tests/test_quicksort.py
+++ tests/test_quicksort.py
@@ -8,3 +8,14 @@ def test_sort_empty_array():
 def test_sort_ascending_input():
     qs = Quicksort()
     assert qs.sort([1,2,3,4]) == [1,2,3,4]
+
+
+def test_sort_descending_input():
+    qs = Quicksort()
+    assert qs.sort([4,3,2,1]) == [1,2,3,4]
+
+
+def test_arbitrary_sequence():
+    qs = Quicksort()
+    # bonus points if you figoure out the meaning of this input :-)
+    assert qs.sort([9, 12, 19, 6]) == [6, 9, 12, 19]
