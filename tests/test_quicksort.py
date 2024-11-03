f03e5173db07f06ea1b535e434092268f762f58e
Frank Schmitt <Frank Schmitt schmitt@quattro-research.com>
2024-11-03 21:11:32 +0100

    unit test: add slightly more complicated example

--- tests/test_quicksort.py
+++ tests/test_quicksort.py
@@ -19,3 +19,8 @@ def test_arbitrary_sequence():
     qs = Quicksort()
     # bonus points if you figoure out the meaning of this input :-)
     assert qs.sort([9, 12, 19, 6]) == [6, 9, 12, 19]
+
+
+def test_alternating():
+    qs = Quicksort()
+    assert qs.sort([1,3,2,5,4,7,6,9,8,10]) == [1,2,3,4,5,6,7,8,9,10]
