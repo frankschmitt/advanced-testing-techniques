1e8dfdec84b3600a1f6991a63698973d372cc1ad
Frank Schmitt <Frank Schmitt schmitt@quattro-research.com>
2024-11-03 21:35:37 +0100

    convert quicksort to stand-alone function

--- advanced_testing_techniques/quicksort.py
+++ advanced_testing_techniques/quicksort.py
@@ -1,18 +1,11 @@
-#import numpy_financial as npf
-
-
-class Quicksort:
-    def __init__(self):
-        pass
-
-    def sort(self, input):
-        """Sort the input list, and return the sorted list (input is unchanged)"""
-        if len(input) <= 1:
-            return input
-        else:
-            pivot = input[0]
-            lt = [x for x in input if x < pivot]
-            gt = [x for x in input if x > pivot]
-            return self.sort(lt) + [pivot] + self.sort(gt)
+def quicksort(input):
+    """Sort the input list, and return the sorted list (input is unchanged)"""
+    if len(input) <= 1:
+        return input
+    else:
+        pivot = input[0]
+        lt = [x for x in input if x < pivot]
+        gt = [x for x in input if x > pivot]
+        return quicksort(lt) + [pivot] + quicksort(gt)
     
     
--- tests/test_quicksort.py
+++ tests/test_quicksort.py
@@ -1,29 +1,20 @@
-from advanced_testing_techniques.quicksort import Quicksort
+from advanced_testing_techniques.quicksort import quicksort
 
 import unittest
 
 class QuicksortTestCase(unittest.TestCase):
     def test_sort_empty_array(self):
-        qs = Quicksort()
-        assert qs.sort([]) == []
-
+        assert quicksort([]) == []
 
     def test_sort_ascending_input(self):
-        qs = Quicksort()
-        assert qs.sort([1,2,3,4]) == [1,2,3,4]
-
+        assert quicksort([1,2,3,4]) == [1,2,3,4]
 
     def test_sort_descending_input(self):
-        qs = Quicksort()
-        assert qs.sort([4,3,2,1]) == [1,2,3,4]
-
+        assert quicksort([4,3,2,1]) == [1,2,3,4]
 
     def test_arbitrary_sequence(self):
-        qs = Quicksort()
         # bonus points if you figoure out the meaning of this input :-)
-        assert qs.sort([9, 12, 19, 6]) == [6, 9, 12, 19]
-
+        assert quicksort([9, 12, 19, 6]) == [6, 9, 12, 19]
 
     def test_alternating(self):
-        qs = Quicksort()
-        assert qs.sort([1,3,2,5,4,7,6,9,8,10]) == [1,2,3,4,5,6,7,8,9,10]
+        assert quicksort([1,3,2,5,4,7,6,9,8,10]) == [1,2,3,4,5,6,7,8,9,10]
